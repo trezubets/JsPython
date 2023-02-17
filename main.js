d5ac6da3f0eed9969dca382c67ba0c40867911ab
trezubets <trezubets993@gmail.com>
2023-02-17 21:44:14 +0600

    change bool to a

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello world!")
+let a = false
+
+if ( a ) {
+    console.log( 'Its true' )
+} else if ( !a ) {
+    console.log( 'Its false' )
+} else {
+    console.log( 'Never happends' )
+}
